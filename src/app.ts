696be164af3c41b480f5236e75108cad6be63c1c
Andy Fillebrown <afillebrown@microsoft.com>
2023-03-17 14:02:47 -0400

    Bootstrap Babylon.js template
    See https://github.com/docEdub/babylonjs-template.

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import * as BABYLON from "babylonjs";
+import { Playground } from "./playground";
+
+(() => {
+    const canvas = <HTMLCanvasElement> document.getElementById("MainCanvas");
+    const engine = new BABYLON.Engine(canvas, true);
+    const scene = Playground.CreateScene(engine, canvas);
+
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
+
+    engine.runRenderLoop(() => {
+       scene.render();
+    });
+})();
