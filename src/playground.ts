aaa675fa6abed4ad14438f27fd4ebf09a7998018
Andy Fillebrown <afillebrown@microsoft.com>
2023-03-17 15:55:40 -0400

    Get GUI multiline animation working

--- src/playground.ts
+++ src/playground.ts
@@ -1,5 +1,3 @@
-import * as BABYLON from "babylonjs"
-
 export
 
 //#region Playground copy/paste code ...
@@ -7,31 +5,28 @@ export
 class Playground {
     public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
         // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
+        const scene = new BABYLON.Scene(engine);
 
         // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+        new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero());
+        // Setup GUI.
+        const gui = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("gui");
 
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
+        // Add a GUI multiline.
+        const line = new BABYLON.GUI.MultiLine();
+        line.add({x: 0, y: 0}, {x: `50%`, y: `50%`});
+        gui.addControl(line);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
-
-        // Our built-in 'sphere' shape. Params: name, options, scene
-        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
-
-        // Our built-in 'ground' shape. Params: name, options, scene
-        var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+        let lineStartX = 0;
+        let lineDeltaX = 1;
+        scene.onAfterRenderObservable.add(() => {
+            lineStartX += lineDeltaX;
+            if (lineStartX <= 0 || 100 <= lineStartX) {
+                lineDeltaX *= -1;
+            }
+            line.getAt(0).x = `${lineStartX}%`;
+        });
 
         return scene;
     }
